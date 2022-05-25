f2561d56d8ed88d1c1b610d8569998f6e1e3fff1
l3z2 <1658441424@qq.com>
2022-05-25 10:17:09 +0800

    修改fff

--- src/com/hz/test/Demo.java
+++ src/com/hz/test/Demo.java
@@ -2,8 +2,6 @@ package com.hz.test;
 
 public class Demo {
     public static void main(String[] args) {
-        System.out.println("=======================");
 
-        System.out.println("jjjjjjjjjjjjjjjj");
     }
 }
